9e99a2ef32f297eb872f4fe267d92e44731049bd
EmJeyDev <emjeydev@gmail.com>
2023-02-05 13:17:05 +0330

    add HelloUser Project

---
+++ HelloUser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Who are you? ");
+            String name = Console.ReadLine();
+            SayHi(name);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(String name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
